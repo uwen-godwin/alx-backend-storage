11c6546847f3d192f6169a7d60311b5423f84d8c
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 13:02:27 +0100

    initial commit

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,13 @@
--- Task 3: Old school band
--- List bands with Glam rock as their main style, ranked by longevity
-
--- Import the table dump
-SOURCE metal_bands.sql;
-
--- Calculate lifespan based on formed and split years
-SELECT band_name, (2022 - formed) AS lifespan
-FROM bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+-- 3-glam_rock.sql
+SELECT 
+    band_name, 
+    CASE
+        WHEN split IS NULL THEN 2022 - formed
+        ELSE split - formed
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    main_style = 'Glam rock'
+ORDER BY 
+    lifespan DESC;
