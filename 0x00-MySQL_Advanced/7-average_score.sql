9a2863af788c4119e0447eb9ca70632b7622ad0c
uwen_godwin <uwengodwin001@gmail.com>
2024-07-12 13:12:31 +0100

    initial commit

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,21 +1,24 @@
 -- 7-average_score.sql
+-- Stored procedure to compute average score for a user
+
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
 
-    -- Calculate average score
+    -- Compute average score for the user
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Update user's average score
+    -- Update average_score in users table
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
+    
+    -- Display computed average score
+    SELECT CONCAT('Average score for user ', (SELECT name FROM users WHERE id = user_id), ': ', avg_score) AS 'Average Score';
 END //
 
 DELIMITER ;
